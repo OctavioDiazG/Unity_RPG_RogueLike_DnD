c0eb90cf6a0207f0295eec75870640648aff50e9
TaquitosDorados <62762581+TaquitosDorados@users.noreply.github.com>
2023-05-16 09:02:33 -0600

    Add Interface for Rollables

--- Assets/_Developers/Dev_PauloAlejandroR_/Scripts/scr_chest.cs
+++ Assets/_Developers/Dev_PauloAlejandroR_/Scripts/scr_chest.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scr_chest : scr_interactables
+public class scr_chest : scr_interactables, IRollable
 {
     public override void Interact()
     {
         base.Interact();
         Debug.Log("cofre");
+        Debug.Log(Roll(20));
+    }
+
+    public int Roll(int diceType)
+    {
+        int rand = Random.Range(1, diceType);
+
+        
+
+        return rand;
     }
 }
---
+++ Assets/_Developers/Dev_PauloAlejandroR_/Scripts/scr_diceRoll.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_diceRoll : MonoBehaviour
+{
+    public static void xd(int diceType, scr_interactables interactingObject)
+    {
+        
+    }
+
+    private static IEnumerator juan()
+    {
+        yield return new WaitForSeconds(2);
+
+
+    }
+}
