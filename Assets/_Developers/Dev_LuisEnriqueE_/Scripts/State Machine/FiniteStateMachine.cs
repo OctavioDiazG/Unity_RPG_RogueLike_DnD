83c3669c788be7437b6ce318b7a57d71d3eaeb26
lemingter <leetv2.1@gmail.com>
2023-05-19 13:30:49 -0600

    FiniteStateMachine stablished

---
+++ Assets/_Developers/Dev_LuisEnriqueE_/Scripts/State Machine/FiniteStateMachine.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiniteStateMachine 
+{
+    public State currentState { get; private set; }
+
+    public void Initialize(State startingState)
+    {
+        currentState = startingState;
+        currentState.Enter();
+    }
+
+    public void ChangeState(State newState)
+    {
+        currentState.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
+}
