4ff8d128a120ddd98d7170a262068ef5918f56a3
OctavioDiazG <odiazg01@outlook.com>
2023-07-24 19:08:44 -0600

    Finish Basic CharacterLocomotion started Weapon System and Character Combat Locomotion

--- Assets/_Developers/Dev_OctavioDiazG_/Weapon/WeaponHolderSlot.cs
+++ Assets/_Developers/Dev_OctavioDiazG_/Weapon/WeaponHolderSlot.cs
@@ -5,25 +5,56 @@ using UnityEngine;
 public class WeaponHolderSlot : MonoBehaviour
 {
     public Transform parentOverride;
-    public bool isLeftHandSlot;
     public bool isRightHandSlot;
+    public bool isLeftHandSlot;
     
     public GameObject currentWeaponModel;
 
-    public void LoadWeaponModel(WeaponItem weaponItem)
-    {
-        if (weaponItem == null)
-        {
-            UnloadWeapon();
-            return;
-        }
-    }
-    
     public void UnloadWeapon()
     {
+        
+        //make animation of Sheathe Weapon
         if (currentWeaponModel != null)
         {
             currentWeaponModel.SetActive(false);
         }
     }
+    
+    public void UnloadWeaponAndDestroy()
+    {
+        //make animation of Sheathe Weapon
+        if (currentWeaponModel != null)
+        {
+            Destroy(currentWeaponModel);
+        }
+    }
+    
+    public void LoadWeaponModel(WeaponItem weaponItem)
+    {
+        //unload current weapon and Destroy it
+        UnloadWeaponAndDestroy();
+        //TODO:make animation of Draw Weapon
+        if (weaponItem == null)
+        {
+            UnloadWeapon();
+            return;
+        }
+        GameObject model = Instantiate(weaponItem.modelPrefab) as GameObject;
+        if(model != null)
+        {
+            if (parentOverride != null)
+            {
+                model.transform.parent = parentOverride;
+            }
+            else
+            {
+                model.transform.parent = transform;
+            }
+            model.transform.localPosition = Vector3.zero;
+            model.transform.localRotation = Quaternion.identity;
+            model.transform.localScale = Vector3.one;
+        }
+        currentWeaponModel = model;
+    }
+    
 }
