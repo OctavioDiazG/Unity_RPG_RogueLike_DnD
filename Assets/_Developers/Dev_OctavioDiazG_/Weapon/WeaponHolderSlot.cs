9de98ab5b69040d81ee172ce0e149d19a76c2fb3
OctavioDiazG <odiazg01@outlook.com>
2023-07-26 11:41:54 -0600

    Added Animations for CombatController

--- Assets/_Developers/Dev_OctavioDiazG_/Weapon/WeaponHolderSlot.cs
+++ Assets/_Developers/Dev_OctavioDiazG_/Weapon/WeaponHolderSlot.cs
@@ -7,8 +7,12 @@ public class WeaponHolderSlot : MonoBehaviour
     public Transform parentOverride;
     public bool isRightHandSlot;
     public bool isLeftHandSlot;
-    
+    public WeaponItem FistWeaponItem;
+
+
     public GameObject currentWeaponModel;
+    
+    public DualWieldAnimationsOverride dualWieldAnimationsOverride;
 
     public void UnloadWeapon()
     {
@@ -26,11 +30,58 @@ public class WeaponHolderSlot : MonoBehaviour
         if (currentWeaponModel != null)
         {
             Destroy(currentWeaponModel);
+            LoadWeaponModel(FistWeaponItem);
         }
     }
     
     public void LoadWeaponModel(WeaponItem weaponItem)
     {
+        List<KeyValuePair<AnimationClip, AnimationClip>> overrideAnim = new();
+        if (dualWieldAnimationsOverride.isDualWield)  //this is not going to work because the variables are not set to true at the same time but in one script and in the other script
+        {
+            //!!!!Currently Not Working!!!!
+            //Create a new overrideAnim List for all the dual wield animations
+            #region Basic Locomotion
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Idle"],dualWieldAnimationsOverride.animations[0]));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Walk"],dualWieldAnimationsOverride.animations[1]));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Run"],dualWieldAnimationsOverride.animations[2]));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Sprint"],dualWieldAnimationsOverride.animations[3]));
+            #endregion
+            #region Rolls
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["RollForward"],dualWieldAnimationsOverride.animations[4]));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["RollBackward"],dualWieldAnimationsOverride.animations[5]));
+            #endregion
+            #region Draw and Sheathe Weapon
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Draw"],dualWieldAnimationsOverride.animations[6]));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Sheathe"],dualWieldAnimationsOverride.animations[7]));
+            #endregion
+            
+            Debug.Log("DualWield Animations Override");
+            
+            weaponItem.animatorOverride.ApplyOverrides(overrideAnim);
+        }
+        else //any other weapons that are not Dual Wield
+        {
+            //!!!!Currently Not Working!!!!
+            #region Basic Locomotion
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Idle"],weaponItem.Idle));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Walk"],weaponItem.Walk));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Run"],weaponItem.Run));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Sprint"],weaponItem.Sprint));
+            #endregion
+            #region Rolls
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["RollForward"],weaponItem.RollForward));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["RollBackward"],weaponItem.RollBackward));
+            #endregion
+            #region Draw and Sheathe Weapon
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Draw"],weaponItem.Draw));
+            overrideAnim.Add(new KeyValuePair<AnimationClip,AnimationClip>(weaponItem.animatorOverride["Sheathe"],weaponItem.Sheathe));
+            #endregion
+            
+            Debug.Log("No DualWield Animations Override");
+            
+            weaponItem.animatorOverride.ApplyOverrides(overrideAnim);
+        }
         //unload current weapon and Destroy it
         UnloadWeaponAndDestroy();
         //TODO:make animation of Draw Weapon
